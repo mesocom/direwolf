e7502568a5e17579f69add59286494b3976c2986
mesocom <devperlaky@gmail.com>
2020-08-20 01:14:08 -0400

    Update morse.h

--- morse.h
+++ morse.h
@@ -1,6 +1,6 @@
 /* morse.h */
 
-int morse_init (struct audio_s *audio_config_p, int amp) ;
+int morse_init (struct audio_s *audio_config_p, int morse_audio_amplitude, int morse_frequency) ;
 
 int morse_send (int chan, char *str, int wpm, int txdelay, int txtail);
 
